1a9fbb305276150257a3e3c93a5badd5b87b65d6
JongminChoi98 <joeychoi7624@gmail.com>
2023-09-07 15:09:16 -0700

    [ADD] audio alarm when work & rest is over with just_audio package.

--- lib/models/timer_model.dart
+++ lib/models/timer_model.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 class TimerModel extends ChangeNotifier {
-  static const _defaultWorkSeconds = 3;
-  static const _defaultRestSeconds = 3;
+  static const _defaultWorkSeconds = 10;
+  static const _defaultRestSeconds = 5;
   static const _defaultPomodoroCount = 0;
   int _remainWorkSeconds = _defaultWorkSeconds;
   int _remainRestSeconds = _defaultRestSeconds;
@@ -19,8 +20,26 @@ class TimerModel extends ChangeNotifier {
   int get remainRestSeconds => _remainRestSeconds;
   int get totalPomodoroCount => _totalPomodoroCount;
 
+  final player = AudioPlayer();
+
+  void _workFinshAudio() async {
+    await player.setAsset("assets/audio/mixkit-alarm-tone-996.wav");
+    player.play();
+  }
+
+  void _restFinshAudio() async {
+    await player.setAsset("assets/audio/mixkit-classic-alarm-995.wav");
+    player.play();
+  }
+
+  // Might need to change to optimize
   void _onTick(Timer timer) {
     if (_remainWorkSeconds == 0) {
+      if (_isRunning) {
+        _isRunning = false;
+        _workFinshAudio();
+      }
+
       if (_remainRestSeconds != 0) {
         if (!_isResting) {
           _totalPomodoroCount = _totalPomodoroCount + 1;
@@ -28,7 +47,9 @@ class TimerModel extends ChangeNotifier {
         _isResting = true;
         _remainRestSeconds = _remainRestSeconds - 1;
       } else {
+        _restFinshAudio();
         _isResting = false;
+        _isRunning = true;
         _remainWorkSeconds = _defaultWorkSeconds;
         _remainRestSeconds = _defaultRestSeconds;
       }
